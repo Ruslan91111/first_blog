6b5bae3ebc80d72c8de4bd5a135870f320132a5d
Ruslan <ruslan_ru911@mail.ru>
2022-09-13 19:47:44 +0300

    2

--- myblog/urls.py
+++ myblog/urls.py
@@ -4,5 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name="article"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
+
 ]
