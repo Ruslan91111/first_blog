4518f2eace242dc31c5c72a465ae01c1fb8d69e0
Ruslan <ruslan_ru911@mail.ru>
2022-09-15 02:26:49 +0300

    15.09.22

--- myblog/models.py
+++ myblog/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='категория')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    title = models.CharField(max_length=255, verbose_name='заголовок')
+    title_tag = models.CharField(max_length=255, verbose_name='Тэг-лайн')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='автор')
+    body = models.TextField(verbose_name='текст')
+    post_date = models.DateField(auto_now_add=True, verbose_name='дата публикации')
+    category = models.CharField(max_length=255, verbose_name='Категория', default='coding')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
