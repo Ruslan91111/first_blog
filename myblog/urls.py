4d24a2a40868ae51cad45641fa68d900ed95cbab
Ruslan <ruslan_ru911@mail.ru>
2022-09-15 13:26:50 +0300

    15.09.2022

--- myblog/urls.py
+++ myblog/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
+    path('category/<str:cats>', category_view, name='category'),
 
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -4,11 +4,23 @@ from .models import *
 from .forms import *
 from django.urls import reverse_lazy
 
+
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     # ordering = ['-id']
     ordering = ['-post_date']
+
+    def get_context_date(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
+
+def category_view(request, cats):
+    category_posts = Post.objects.filter(category=cats.replace('-', ' '))
+    return render(request, 'categories.html', {'cats': cats.title().replace('-', ' '), 'category_posts': category_posts})
 
 
 class ArticleDetailView(DetailView):
@@ -21,6 +33,13 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__'
+
+
+class AddCategoryView(CreateView):
+    model = Category
+    # form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '__all__'
 
 
 class UpdatePostView(UpdateView):
